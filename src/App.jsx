4246ec17cbef1fcbf8f324621ff200b7d00e85d2
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-06 10:00:52 +0530

    feat: Search box ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import CarCard from './components/CarCard'
 
+// To add
+// Radio button options for Transmission (Automatic / Manual / All), Type (Economy / Sedan/ SUV / Luxury / All),
+// Drop down for Sort by price: Low→High and High→Low.
+// Results counter: "Showing X of Y cars", updating live.
+// Search box
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='my-14 mx-20'>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <div>
+        <div className='my-5'>
+          <input type="text" placeholder='Search...' className='border-2 border-gray-200 border-solid rounded-md px-3 py-2 w-3/4' />
+        </div>
+      </div>
       <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-y-6 gap-x-6'>
         <CarCard/>
         <CarCard/>
