df2b969ea69a649db1194ca9922a0d395d3732df
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-06 09:43:29 +0530

    feat: grid logic for cards implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
+import CarCard from './components/CarCard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </>
+      <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-y-6 gap-x-6'>
+        <CarCard/>
+        <CarCard/>
+        <CarCard/>
+        <CarCard/>
+        <CarCard/>
+        <CarCard/>
+        <CarCard/>
+      </div>
+    </div>
   )
 }
 
