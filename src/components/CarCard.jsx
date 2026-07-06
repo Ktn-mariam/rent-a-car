73de658b4b4019486eb77e9990020f5de9f995a1
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-06 12:28:15 +0530

    fix: ui of car card improved

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -14,18 +14,18 @@ const CarCard = ({ car }) => {
           <img src="https://media.wired.com/photos/63b8d0a771c6b526845f15a6/3:2/w_2400,h_1600,c_limit/CES-2023-PEUGEOT_INCEPTION_CONCEPT_2301CN202.jpg" alt="Car" className="object-cover w-full h-full" />
         </div>
       </div>
-              <div className="flex gap-3">
-          <div className="flex items-center gap-0.5">
+      <div className="flex gap-2">
+          <div className="flex items-center gap-0.5 border-r-2 border-solid border-gray-300 pr-2">
             <FaGear />
             <p className="p-0 m-0 leading-none">{transmission}</p>
           </div>
-          <div className="flex items-center gap-0.5">
+          <div className="flex items-center gap-0.5 border-r-2 border-solid border-gray-300 pr-2">
             <MdEventSeat />
             <p className="p-0 m-0 leading-none">{seats}</p>
           </div>
           {(available) ? <p className="italic text-green-600">Available</p> :
           <p className="italic text-red-600">Not Available</p>}
-        </div>
+      </div>
       <div>
         Price: <span className="font-semibold">{pricePerDay}</span> AED/ Day
       </div>
