571e1aca48c20937a50986d180345bf5b35f1fd6
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-07 10:50:10 +0530

    feat: images added to the data

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -2,7 +2,7 @@ import { MdEventSeat } from "react-icons/md";
 import { FaGear } from "react-icons/fa6";
 
 const CarCard = ({ car }) => {
-  const { name, type, transmission, seats, available, pricePerDay } = car;
+  const { name, type, transmission, seats, available, pricePerDay, image } = car;
   return (
     <div className="w-fit px-3 rounded-md py-3">
       <div className="flex items-end justify-between">
@@ -11,7 +11,7 @@ const CarCard = ({ car }) => {
       </div>
       <div className="flex items-center justify-center mt-2">
         <div className="h-40 w-64 mb-3 flex items-center justify-center overflow-hidden rounded-sm">
-          <img src="https://media.wired.com/photos/63b8d0a771c6b526845f15a6/3:2/w_2400,h_1600,c_limit/CES-2023-PEUGEOT_INCEPTION_CONCEPT_2301CN202.jpg" alt="Car" className="object-cover w-full h-full" />
+          <img src={image} alt="Car" className="object-cover w-full h-full" />
         </div>
       </div>
       <div className="flex gap-2">
