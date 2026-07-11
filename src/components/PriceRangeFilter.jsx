3b25efefee9d65016be269811bc9dd10bbd36bb8
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-11 20:25:38 +0530

    feat: error messages added for price range filter, and input type changed to number

--- src/components/PriceRangeFilter.jsx
+++ src/components/PriceRangeFilter.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
 
 const PriceRangeFilter = ({lowerRange, upperRange, handleParamChange}) => {
+  const [error, setError] = useState(null)
   const handleLowerRangeInputChange = (event) => {
     const value = event.target.value
+    if (value && upperRange && value > Number(upperRange)) {
+      setError("Lower range cannot be greater than upper range")
+      return
+    }
+    if (value && value < 0) {
+      setError("Price cannot be less than 0")
+      return
+    }
+    setError(null)
     if (value) {
       handleParamChange("lowerPriceRange", value)
       return
@@ -12,6 +22,15 @@ const PriceRangeFilter = ({lowerRange, upperRange, handleParamChange}) => {
 
   const handleUpperRangeInputChange = (event) => {
     const value = event.target.value
+    if (value && lowerRange && value < Number(lowerRange)) {
+      setError("Upper range cannot be less than lower range")
+      return
+    }
+    if (value && value < 0) {
+      setError("Price cannot be less than 0")
+      return
+    }
+    setError(null)
     if (value) {
       handleParamChange("upperPriceRange", value)
       return
@@ -23,10 +42,11 @@ const PriceRangeFilter = ({lowerRange, upperRange, handleParamChange}) => {
     <div>
       <h4 className='border-b-2 border-solid border-gray-200 mb-2 font-semibold'>Price Per Day</h4>
       <div className='flex items-center gap-2'>
-        <input defaultValue={lowerRange} onChange={handleLowerRangeInputChange} className='border-2 border-zinc-200 w-14 rounded-md px-2' />
+        <input placeholder='min' type='number' defaultValue={lowerRange} onChange={handleLowerRangeInputChange} className='border-2 border-zinc-200 w-16 rounded-lg px-2' />
         to
-        <input defaultValue={upperRange} onChange={handleUpperRangeInputChange} className='border-2 border-zinc-200 w-14 rounded-md px-2'/>
+        <input placeholder='max' type='number' defaultValue={upperRange} onChange={handleUpperRangeInputChange} className='border-2 border-zinc-200 w-16 rounded-lg px-2'/>
       </div>
+      {error && <p className='text-red-500 text-xs mt-1'>{error}</p>}
     </div>
   )
 }
