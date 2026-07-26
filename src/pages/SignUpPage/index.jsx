732545e3636ff689d519ef453f189881b5e72709
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-26 12:48:23 +0400

    feat: sign up page now functional

--- src/pages/SignUpPage/index.jsx
+++ src/pages/SignUpPage/index.jsx
@@ -1,28 +1,55 @@
 import { MdAccountCircle, MdEmail } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
+import { useRef, useContext } from "react";
+import AuthenticationContext from "../../context/auth";
+import { useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
+  const { signUpHandler } = useContext(AuthenticationContext);
+  const nameRef = useRef(null)
+  const emailRef = useRef(null)
+  const passwordRef = useRef(null)
+  const retypePasswordRef = useRef(null)
+
+  const navigate = useNavigate();
+
+  const InputSubmitHandler = (event) => {
+    event.preventDefault()
+    const nameInput = nameRef.current.value;
+    const emailInput = emailRef.current.value;
+    const passwordInput = passwordRef.current.value;
+    const retypePasswordInput = retypePasswordRef.current.value;
+
+    if (passwordInput !== retypePasswordInput) {
+      return
+    }
+
+    signUpHandler(nameInput, emailInput, passwordInput);
+
+    navigate("/");
+  }
+  
   return (
     <div className='flex items-center justify-center'>
       <form className='flex flex-col mt-20 border-2 border-solid border-zinc-200 px-8 py-10 rounded-3xl gap-7 lg:w-1/3 w-96'>
         <h1 className='text-2xl font-bold self-center'>Create Account</h1>
         <div className='flex gap-2 items-center border-2 border-solid border-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors'>
           <MdAccountCircle size={'1.3rem'} className="text-zinc-300"/>
-          <input type="text" className='py-1 w-full border-none focus:outline-none' placeholder='Full Name'/>
+          <input type="text" className='py-1 w-full border-none focus:outline-none' ref={nameRef} placeholder='Full Name'/>
         </div>
         <div className='flex gap-2 items-center border-2 border-solid border-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors'>
           <MdEmail size={'1.3rem'} className="text-zinc-300"/>
-          <input type="text"  className='py-1 w-full border-none focus:outline-none' placeholder='Email'/>
+          <input type="text" ref={emailRef}  className='py-1 w-full border-none focus:outline-none' placeholder='Email'/>
         </div>
         <div className='flex gap-2 items-center border-2 border-solid border-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors'>
           <FaLock size={'1rem'} className="text-zinc-300"/>
-          <input type="password"  className='py-1 w-full border-none focus:outline-none' placeholder='Password'/>
+          <input type="password" ref={passwordRef}  className='py-1 w-full border-none focus:outline-none' placeholder='Password'/>
         </div>
         <div className='flex gap-2 items-center border-2 border-solid border-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors'>
           <FaLock size={'1rem'} className="text-zinc-300"/>
-          <input type="password"  className='py-1 w-full border-none focus:outline-none' placeholder='Retype Password'/>
+          <input type="password" ref={retypePasswordRef}  className='py-1 w-full border-none focus:outline-none' placeholder='Retype Password'/>
         </div>
-        <button className='bg-black text-white py-2 rounded-md'>Sign up</button>
+        <button onClick={InputSubmitHandler} className='bg-black text-white py-2 rounded-md'>Sign up</button>
       </form>
     </div>
   )
