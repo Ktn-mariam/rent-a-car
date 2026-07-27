d623ef00fc601788c12f6e11984be242ecc07713
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-27 19:32:02 +0400

    feat: driverDetails ui created

--- src/pages/BookingPage/DriverDetails.jsx
+++ src/pages/BookingPage/DriverDetails.jsx
@@ -1,8 +1,38 @@
+import { useRef } from "react"
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
-const DriverDetails = () => {
+const DriverDetails = ({setWizardStepNumber}) => {
+  const nameRef = useRef(null)
+  const mobileNoRef = useRef(null)
+  const LicenseRef = useRef(null)
+
   return (
-    <div>
-      Driver Details
+    <div className="w-full">
+      <p>Confirm Driver Details:</p>
+      <form className="mt-5 flex gap-3 w-full" action="">
+        <div className="flex flex-col gap-1 flex-1">
+          <label htmlFor="">Full Name</label>
+          <input type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1' placeholder='Ex: John Max'/>
+        </div>
+        <div className="flex flex-col gap-1 flex-1">
+          <label htmlFor="">Mobile No.</label>
+          <input type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1' placeholder='Ex: example@gmail.com'/>
+        </div>
+        <div className="flex flex-col gap-1 flex-1">
+          <label htmlFor="">License Number:</label>
+          <input type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1' placeholder='Ex: 123456789'/>
+        </div>
+      </form>
+      <div className="flex gap-3 items-end justify-end">
+        <button onClick={()=> setWizardStepNumber(1)} className='flex items-center gap-4 bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer mt-7'>
+          <FaArrowLeft />
+          <p>Prev</p>
+        </button>
+        <button onClick={()=> setWizardStepNumber(2)} className='flex items-center gap-4 bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer mt-7'>
+          <p>Next</p>
+          <FaArrowRight />
+        </button>
+      </div>
     </div>
   )
 }
