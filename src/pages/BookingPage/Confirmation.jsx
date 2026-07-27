e62b27d967fdfa0c9d275d58f0f6be710073e602
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-27 21:47:08 +0400

    feat: confirmation page ui added

--- src/pages/BookingPage/Confirmation.jsx
+++ src/pages/BookingPage/Confirmation.jsx
@@ -1,8 +1,48 @@
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 
-const Confirmation = () => {
+const Confirmation = ({setWizardStepNumber}) => {
+  const handleConfirmation = () => {}
+
   return (
     <div>
-      Confirmation
+      <p className="mb-3">Confirm Booking Details:</p>
+      <p className="border-b-2 border-zinc-200 mb-3">Selected Dates</p>
+      <div className="flex gap-10">
+        <div className="flex flex-col gap-1 flex-1">
+          <label htmlFor="">From:</label>
+          <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>24-45-2019</div>
+        </div>
+        <div className="flex flex-col gap-1 flex-1">
+          <label htmlFor="">To:</label>
+          <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>24-45-2019</div>
+        </div>
+      </div>
+      <div className="mt-5">
+        <p className="border-b-2 border-zinc-200 mb-3">Driver Details</p>
+        <div className="flex gap-3 w-full">
+          <div className="flex flex-col gap-1 flex-1">
+            <label htmlFor="">Full Name</label>
+            <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>Mariam Khatoon</div>
+          </div>
+          <div className="flex flex-col gap-1 flex-1">
+            <label htmlFor="">Mobile No.</label>
+            <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1' placeholder='Ex: example@gmail.com'>903209329032</div>
+          </div>
+          <div className="flex flex-col gap-1 flex-1">
+            <label htmlFor="">License Number:</label>
+            <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>93990e32092</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-3 items-end justify-end">
+        <button onClick={()=> setWizardStepNumber(2)} className='flex items-center gap-2 bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer mt-7'>
+          <FaArrowLeft />
+          <p>Back</p>
+        </button>
+        <button onClick={handleConfirmation} className='bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer mt-7'>
+          <p>Confirm Booking</p>
+        </button>
+      </div>
     </div>
   )
 }
