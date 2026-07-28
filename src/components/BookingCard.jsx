0d0fe2040db48db7b9b2e3670673bb0d3ce967dd
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-28 18:04:45 +0400

    feat: MyBookings Page now functional

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -1,20 +1,37 @@
+import { formatDate } from "../helpers/dateFormatting"
 
 const BookingCard = ({ startDate, endDate, driverInformation }) => {
+  let displayStartDate;
+  let displayEndDate;
+
+  if (startDate instanceof Date && !isNaN(startDate.getTime())) {
+    displayStartDate = formatDate(startDate)
+  } else {
+    displayStartDate = startDate
+  }
+
+  if (endDate instanceof Date && !isNaN(endDate.getTime())) {
+    displayEndDate = formatDate(endDate)
+  } else {
+    displayEndDate = endDate
+  }
+  console.log(endDate);
+  
   return (
     <div>
-      <p className="border-b-2 border-zinc-200 mb-3">Selected Dates</p>
+      <p className="font-semibold">Selected Dates</p>
       <div className="flex gap-3">
         <div className="flex flex-col gap-1 flex-1">
           <label htmlFor="">From:</label>
-          <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>{formatDate(startDate)}</div>
+          <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>{displayStartDate}</div>
         </div>
         <div className="flex flex-col gap-1 flex-1">
           <label htmlFor="">To:</label>
-          <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>{formatDate(endDate)}</div>
+          <div type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1'>{displayEndDate}</div>
         </div>
       </div>
       <div className="mt-5">
-        <p className="border-b-2 border-zinc-200 mb-3">Driver Details</p>
+        <p className="font-semibold">Driver Details</p>
         <div className="flex gap-3 w-full">
           <div className="flex flex-col gap-1 flex-1">
             <label htmlFor="">Full Name</label>
--- src/pages/MyBookingsPage/index.jsx
+++ src/pages/MyBookingsPage/index.jsx
@@ -1,8 +1,56 @@
 
+import { useContext, useEffect, useState } from "react"
+import BookingsContext from "../../context/bookings"
+import BookingCard from "../../components/BookingCard"
+
 const MyBookingsPage = () => {
+  const { bookings } = useContext(BookingsContext)
+  const [pastBookings, setPastBookings] = useState(null)
+  const [upcomingBookings, setUpcomingBookings] = useState(null)
+  const [sideBarOption, setSideBarOption] = useState("Past")
+
+  useEffect(()=>{
+    const pastBookingsFiltered = bookings.filter((booking)=> {
+      const bookingEndDate = new Date(booking.endDate)
+      const todayDate = new Date()
+      return bookingEndDate < todayDate
+    })
+
+    const upcomingBookingsFiltered = bookings.filter((booking)=> {
+      const bookingEndDate = new Date(booking.endDate)
+      const todayDate = new Date()
+      return bookingEndDate > todayDate
+    })
+
+    setPastBookings(pastBookingsFiltered)
+    setUpcomingBookings(upcomingBookingsFiltered)
+  }, [bookings])
+
   return (
-    <div>
-      My Bookings
+    <div className="mb-14 ">
+      <h1 className="sm:mx-20 mx-5 mb-10 font-bold text-xl italic">My Bookings</h1>
+      <div className="flex sm:gap-10 gap-5 my-5 sm:mx-20 mx-5">
+        <div className="flex flex-col sm:w-56 min-w-40 gap-3">
+          <button className="text-left px-5 py-2 rounded-md bg-zinc-100" onClick={()=> setSideBarOption("Past")}>Past</button>
+          <button className="text-left px-5 py-2 rounded-md bg-zinc-100" onClick={()=> setSideBarOption("Upcoming")}>Upcoming</button>
+        </div>
+        <div className="flex flex-col flex-1 gap-20">
+          {sideBarOption === "Upcoming" && upcomingBookings && upcomingBookings.map((booking, index)=>{
+            return (
+            <div key={index}>
+              <h1 className="border-b-2 border-zinc-200 font-bold">Booking ID: {booking.id}</h1>
+              <BookingCard startDate={booking.startDate} endDate={booking.endDate} driverInformation={booking.driver}/>
+            </div>
+          )})}
+          {sideBarOption === "Past" && pastBookings && pastBookings.map((booking, index)=>{
+            return (
+            <div key={index}>
+              <h1 className="border-b-2 border-zinc-200 font-bold">Booking ID: {booking.id}</h1>
+              <BookingCard startDate={booking.startDate} endDate={booking.endDate} driverInformation={booking.driver}/>
+            </div>
+          )})}
+        </div>
+      </div>
     </div>
   )
 }
