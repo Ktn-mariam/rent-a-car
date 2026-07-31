5e2c02bd7ca44a54fe2edde925e50c055eb7c633
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-31 16:54:51 +0400

    feat: modal confirming deletion of booking added

--- src/pages/MyBookingsPage/index.jsx
+++ src/pages/MyBookingsPage/index.jsx
@@ -4,13 +4,16 @@ import BookingsContext from "../../context/bookings"
 import BookingCard from "../../components/BookingCard"
 import AuthenticationContext from "../../context/auth"
 import { useNavigate } from "react-router-dom"
+import Modal from "../../components/Modal"
+import { GoAlertFill } from "react-icons/go";
 
 const MyBookingsPage = () => {
-  const { bookings } = useContext(BookingsContext)
+  const { bookings, setBookings } = useContext(BookingsContext)
   const { logInData } = useContext(AuthenticationContext)
   const [pastBookings, setPastBookings] = useState(null)
   const [upcomingBookings, setUpcomingBookings] = useState(null)
   const [sideBarOption, setSideBarOption] = useState("Upcoming")
+  const [bookingToCancel, setBookingToCancel] = useState(null);
 
   const navigate = useNavigate()
 
@@ -31,12 +34,35 @@ const MyBookingsPage = () => {
     setUpcomingBookings(upcomingBookingsFiltered)
   }, [bookings])
 
+  const handleCancelBooking = (id) => {
+    const newBookings = bookings.filter((booking)=>{
+      return booking.id !== id
+    })
+
+    setBookings(newBookings)
+  }
+
+
   if (!logInData.isLoggedIn) {
     navigate("/login")
   }
 
   return (
     <div className="mb-14 ">
+      <Modal isOpen={bookingToCancel !== null} setIsOpen={() => setBookingToCancel(null)}>
+        <div className="flex items-center flex-col gap-2">
+          <GoAlertFill className="text-red-600" size={'1.5rem'}/>
+          <p className="text-red-600">Are you sure you want to delete booking No. {bookingToCancel?.id}?</p>
+          <div className="flex gap-2 mt-7">
+            <button onClick={() => setBookingToCancel(null)} className="bg-zinc-300 rounded-md px-3 py-1 hover:cursor-pointer">Cancel</button>
+            <button onClick={() => {
+              handleCancelBooking(bookingToCancel?.id);
+              setBookingToCancel(null);
+            }} 
+              className="bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer">Confirm</button>
+          </div>
+        </div>
+      </Modal>
       <h1 className="sm:mx-20 mx-5 mb-10 font-bold text-xl italic">My Bookings</h1>
       <div className="flex sm:gap-10 gap-5 my-5 sm:mx-20 mx-5">
         <div className="flex flex-col sm:w-56 min-w-40 gap-3">
@@ -50,7 +76,7 @@ const MyBookingsPage = () => {
               <h1 className="border-b-2 border-zinc-200 font-bold">Booking ID: {booking.id}</h1>
               <BookingCard startDate={booking.startDate} endDate={booking.endDate} driverInformation={booking.driver}/>
               <div className="flex justify-end mt-5">
-                <button className="bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer hover:bg-red-700">Cancel Booking</button>
+                <button onClick={() => setBookingToCancel(booking)} className="bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer hover:bg-red-700">Cancel Booking</button>
               </div>
             </div>
           )})}
