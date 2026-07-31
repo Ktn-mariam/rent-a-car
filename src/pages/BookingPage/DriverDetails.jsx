bc53e20188079c3ed4b581632ac574dc64512b09
Mariam Khatoon <mariamkhatoon996@gmail.com>
2026-07-31 16:07:36 +0400

    feat: error handler for if user does not enter all fields

--- src/pages/BookingPage/DriverDetails.jsx
+++ src/pages/BookingPage/DriverDetails.jsx
@@ -1,15 +1,23 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { MdError } from "react-icons/md";
 
 const DriverDetails = ({setWizardStepNumber, driverInformation, setDriverInformation}) => {
+  const [error, setError] = useState(null)
   const nameRef = useRef(null)
   const mobileNoRef = useRef(null)
   const licenseRef = useRef(null)
 
   const handleNextButton = () => {
+    setError(null)
     const nameInput = nameRef.current.value;
     const mobileNoInput = mobileNoRef.current.value;
     const licenseInput = licenseRef.current.value;
+
+    if (!nameInput || !mobileNoInput || !licenseInput) {
+      setError("Please enter all fields")
+      return
+    }
 
     const DriverInfo = {name: nameInput, mobileNo: mobileNoInput, licenseNo: licenseInput}
 
@@ -34,6 +42,11 @@ const DriverDetails = ({setWizardStepNumber, driverInformation, setDriverInforma
           <input type="text" className='border-2 border-solid bg-zinc-200 rounded-md px-2 focus-within:border-gray-300 transition-colors py-1' ref={licenseRef} defaultValue={driverInformation?.licenseNo || null} placeholder='Ex: 123456789'/>
         </div>
       </form>
+      {error && 
+        <div className="flex gap-2 items-center mt">
+          <MdError className="text-red-600" />
+          <p className="text-red-600">Error: {error}</p>
+        </div>}
       <div className="flex gap-3 items-end justify-end">
         <button onClick={()=> setWizardStepNumber(1)} className='flex items-center gap-4 bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer mt-7'>
           <FaArrowLeft />
